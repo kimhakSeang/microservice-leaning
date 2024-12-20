d03b8ad6e3d7b192a005614ebd65442bf800bf32
Kimhak Seang <tthyu83@gmail.com>
2024-12-21 00:48:48 +0700

    Week_22_Health_Check-Docker_Compose

--- account/src/main/java/com/piseth/bank/account/service/LoanService.java
+++ account/src/main/java/com/piseth/bank/account/service/LoanService.java
@@ -1,16 +1,14 @@
 package com.piseth.bank.account.service;
 
 import com.piseth.bank.account.dto.LoanDTO;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
-@FeignClient(name = "loan")
+//@FeignClient(name = "loan")
 public interface LoanService {
 
-    @GetMapping(value = "/api/loans/{customerId}")
+//    @GetMapping(value = "/api/loans/{customerId}")
     List<LoanDTO> getLoanInfoByCustomerId(@RequestHeader("connector-id") String connectorId, @PathVariable  Integer customerId);
 }
