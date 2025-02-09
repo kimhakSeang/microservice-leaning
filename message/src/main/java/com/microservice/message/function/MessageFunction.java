cf2653714bca1f325c4136cf382909233b2f51c1
Kimhak Seang <tthyu83@gmail.com>
2025-02-09 23:15:48 +0700

    Week_34_Spring_Cloud-Eror_Lombok

--- message/src/main/java/com/microservice/message/function/MessageFunction.java
+++ message/src/main/java/com/microservice/message/function/MessageFunction.java
@@ -1,18 +1,20 @@
 package com.microservice.message.function;
 
+import lombok.extern.slf4j.Slf4j;
 import com.microservice.message.dto.CustomerMessageDto;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Function;
 
+@Slf4j
 @Configuration
 public class MessageFunction {
 
     @Bean
     public Function<CustomerMessageDto, String> email(){
-            return customerMessage -> customerMessage.getEmail();
+        log.info("Start sending message to email....");
+        return CustomerMessageDto::getEmail;
     }
 
 }
