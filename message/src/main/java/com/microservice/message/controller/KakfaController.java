3062b6433a6b09a3be4e3e8453a9a3fc1131ba0d
Kimhak Seang <tthyu83@gmail.com>
2025-03-11 22:54:57 +0700

    🚀 WEEK_40_Kubernetes_Configserver
    Kubernetes Configserver

---
+++ message/src/main/java/com/microservice/message/controller/KakfaController.java
@@ -0,0 +1,22 @@
+package com.microservice.message.controller;
+
+import com.microservice.message.dto.CustomerMessageDto;
+import com.microservice.message.function.KafkaTopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KakfaController {
+//    @Autowired
+//    private KafkaTopicService kafkaTopicService;
+
+    @PostMapping("")
+    public ResponseEntity<?> sendTopic(CustomerMessageDto customerMessageDto){
+//        kafkaTopicService.send(customerMessageDto);
+        return ResponseEntity.ok("Work");
+    }
+}
